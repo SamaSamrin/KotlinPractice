857ed26efdf51d44f51b7b255816ecc3e0da664f
Sama Samrin <samasamrin@gmail.com>
2017-05-23 22:13:10 +0600

    added textView

--- app/src/main/java/com/example/user/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/user/kotlinpractice/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.user.kotlinpractice
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+//import below stuffs for layouts/xml
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv1.setText("New Text")
+        tv2.setText("New Sama")
+        val color = Color.CYAN;
+        tv1.setBackgroundColor(color)
     }
+
+
 }
+
